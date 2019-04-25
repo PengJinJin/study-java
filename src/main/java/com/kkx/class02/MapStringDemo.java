44fd837a42154984001614095f41ea34cb23c51d
PengJinJin <1057486157@qq.com>
2019-04-25 15:39:40 +0800

    使用IO读取文本文件

--- src/main/java/com/kkx/class02/MapStringDemo.java
+++ src/main/java/com/kkx/class02/MapStringDemo.java
@@ -1,12 +1,29 @@
 package com.kkx.class02;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class MapStringDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		// 现在需要做的是: 使用IO流读取一个小说,然后再进行下面的一步
+
+		// 使用输入字符流来进行读取文件的操作
+		InputStreamReader isr = new InputStreamReader(new FileInputStream("MapStringDemo.txt"), "UTF-8");
+		BufferedReader reader = new BufferedReader(isr);
+		StringBuilder builder = new StringBuilder();
+		String str;
+
+		while ((str = reader.readLine()) != null) {
+			builder.append(str);
+		}
+
 		// 一个字符串，每个字符出现的次数，按出现次数顺序排列
-		String s = "woaizhuzhuhequanquan";
+//		String s = "woaizhuzhuhequanquan";
+		String s = builder.toString();
 		Map<Character, Integer> map = new HashMap<Character, Integer>();
 		for (int i = 0; i < s.length(); i++) {
 			char c = s.charAt(i);
